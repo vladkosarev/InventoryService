898a3bf02a8af82fb480146a71eea9c4da1a83f7
Vlad Kosarev <vlad@webmustflow.com>
2016-05-20 13:36:08 -0400

    Improved InMemory storage speed

--- InventoryService.Storage.InMemory/InMemory.cs
+++ InventoryService.Storage.InMemory/InMemory.cs
@@ -1,61 +1,23 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace InventoryService.Storage
 {
     public class InMemory : IInventoryStorage
     {
-        private readonly Dictionary<string, Tuple<int, int>> _productInventories =
-            new Dictionary<string, Tuple<int, int>>();
-
-        public async Task<int> ReadQuantity(string productId)
-        {
-            if (!_productInventories.ContainsKey(productId))
-                throw new InvalidOperationException();
-            return _productInventories[productId].Item1;
-        }
-
-        public async Task<int> ReadReservations(string productId)
-        {
-            if (!_productInventories.ContainsKey(productId))
-                throw new InvalidOperationException();
-            return _productInventories[productId].Item2;
-        }
+        private readonly ConcurrentDictionary<string, Tuple<int, int>> _productInventories =
+            new ConcurrentDictionary<string, Tuple<int, int>>();
 
         public async Task<Tuple<int, int>> ReadInventory(string productId)
         {
-            if (!_productInventories.ContainsKey(productId))
-                throw new InvalidOperationException();
             return _productInventories[productId];
-        }
-
-        public async Task<bool> WriteQuantity(string productId, int quantity)
-        {
-            if (!_productInventories.ContainsKey(productId))
-                _productInventories.Add(productId, new Tuple<int, int>(quantity, 0));
-            else
-                _productInventories[productId] = new Tuple<int, int>(quantity, _productInventories[productId].Item2);
-            return true;
-        }
-
-        public async Task<bool> WriteReservations(string productId, int reservationQuantity)
-        {
-            if (!_productInventories.ContainsKey(productId))
-                _productInventories.Add(productId, new Tuple<int, int>(0, reservationQuantity));
-            else
-                _productInventories[productId] = new Tuple<int, int>(_productInventories[productId].Item1,
-                    reservationQuantity);
-            return true;
         }
 
         public async Task<bool> WriteInventory(string productId, int quantity, int reservationQuantity)
         {
-            if (!_productInventories.ContainsKey(productId))
-                _productInventories.Add(productId, new Tuple<int, int>(quantity, reservationQuantity));
-            else
-                _productInventories[productId] = new Tuple<int, int>(quantity, reservationQuantity);
+            _productInventories.AddOrUpdate(productId, new Tuple<int, int>(quantity, reservationQuantity),
+                (key, oldValue) => new Tuple<int, int>(quantity, reservationQuantity));
             return true;
         }
 
