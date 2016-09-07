c7639fa053315c8558b89b2c538980a80c96d7c8
Sam <contactsamie@gmail.com>
2016-09-07 13:43:14 -0400

    update client

--- InventoryService.Console/SampleClientClass.cs
+++ InventoryService.Console/SampleClientClass.cs
@@ -15,7 +15,7 @@ namespace InventoryService.Console
         public async Task StartSampleClientAsync()
         {
             const int productCount = 100;
-            const int initialQuantity = 1000;
+            const int initialQuantity = 1;
 
             IList<Tuple<string, int, int>> products = new List<Tuple<string, int, int>>();
             for (var product = 0; product < productCount; product++)
@@ -24,7 +24,7 @@ namespace InventoryService.Console
             }
 
             System.Console.WriteLine("Starting Client");
-            using (var actorSystem = ActorSystem.Create("InventoryService-Client"))
+            var actorSystem = ActorSystem.Create("InventoryService-Client");
             {
                 var remoteAddress = ConfigurationManager.AppSettings["RemoteActorAddress"];
                 var inventoryActorSelection =
@@ -36,41 +36,42 @@ namespace InventoryService.Console
 
                 stopwatch.Start();
 
-                Task.WaitAll(products.Select(p =>
+                Task.WaitAll(products.Select(async p =>
                 {
-                    return Task.Run(async () =>
+                    for (var i = 0; i < initialQuantity; i++)
                     {
-                        for (var i = 0; i < initialQuantity; i++)
+                        try
                         {
-                            try
-                            {
-                                var update =
-                                    await
-                                        inventoryActor.Ask<UpdateQuantityCompletedMessage>(
-                                            new UpdateQuantityMessage(p.Item1, 1), TimeSpan.FromSeconds(10));
-                                if (update == null) throw new Exception("remote actor returned null message");
-                                System.Console.WriteLine("Updating  item {0}'s quantity for product {0}", i, p.Item1);
-                            }
-                            catch (Exception ex)
-                            {
-                                System.Console.WriteLine("Failed on iteration {0} while updating quantity {1} : {2}", i, p.Item1,
-                                    ex.Message + " - " + ex);
-                            }
-
-                            var reservation =
+                            var update =
                                 await
-                                    inventoryActor.Ask<ReserveCompletedMessage>(new ReserveMessage(p.Item1, 1),
-                                        TimeSpan.FromSeconds(10));
-                            System.Console.WriteLine(
-                                !reservation.Successful ? "Failed on iteration {0}" : "Reserved item {0} successfully ", i);
+                                    inventoryActor.Ask<UpdateQuantityCompletedMessage>(
+                                        new UpdateQuantityMessage(p.Item1, 1), TimeSpan.FromSeconds(10)).ConfigureAwait(false);
+                            if (update == null) throw new Exception("remote actor returned null message");
+                            System.Console.WriteLine("Updating  item {0}'s quantity for product {0}", i, p.Item1);
                         }
-                    });
-                }).ToArray());
+                        catch (Exception ex)
+                        {
+                            System.Console.WriteLine("Failed on iteration {0} while updating quantity {1} : {2}", i, p.Item1,
+                                ex.Message + " - " + ex);
+                        }
 
+                        var reservation =
+                            await
+                                inventoryActor.Ask<ReserveCompletedMessage>(new ReserveMessage(p.Item1, 1),
+                                    TimeSpan.FromSeconds(10)).ConfigureAwait(false);
+                        System.Console.WriteLine(
+                            !reservation.Successful ? "Failed on iteration {0}" : "Reserved item {0} successfully ", i);
+                    }
+
+                   // await Task.Delay(TimeSpan.FromSeconds(10));
+                }).ToArray());
+               // Task.WaitAll(actorSystem.Terminate());
                 stopwatch.Stop();
 
                 System.Console.WriteLine("Elapsed: {0}", stopwatch.Elapsed.TotalSeconds);
                 System.Console.WriteLine("Speed: {0} per second", productCount * initialQuantity / stopwatch.Elapsed.TotalSeconds);
+
+              
             }
         }
     }
