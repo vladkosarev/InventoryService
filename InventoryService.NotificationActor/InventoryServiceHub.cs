8c6c2ba3c7d867dd232f0a1ce2a10e3a229ae4c3
Sam <contactsamie@gmail.com>
2016-11-02 12:49:45 -0400

    update build

--- InventoryService.NotificationActor/InventoryServiceHub.cs
+++ InventoryService.NotificationActor/InventoryServiceHub.cs
@@ -10,35 +10,51 @@ namespace InventoryService.NotificationActor
     public class InventoryServiceHub : Hub
     {
         private static readonly Logger Log = LogManager.GetCurrentClassLogger();
-        private ActorSystem HubActorSystem { set; get; }
+        private static ActorSystem HubActorSystem { set; get; }
 
         public void GetInventoryList()
         {
             var actor = GetRemoteActor();
             actor.Tell(new QueryInventoryListMessage());
         }
+        
 
         private ActorSelection GetRemoteActor()
         {
             if (HubActorSystem == null)
             {
                 var name = "SignalRHubActorSystem";
-                Log.Debug("Creating actor system :" + name);
-                HubActorSystem = ActorSystem.Create(name, @"
+                Log.Debug("Trying to create notification hub actor system :" + name);
+                HubActorSystem = HubActorSystem?? ActorSystem.Create(name, @"
                    akka {
-                   loggers = [""Akka.Logger.NLog.NLogLogger,Akka.Logger.NLog""]
-                   stdout-loglevel = DEBUG
-                   loglevel = DEBUG
-                   log-config-on-start = on
+                   #loggers = [""Akka.Logger.NLog.NLogLogger,Akka.Logger.NLog""]
+                   #stdout-loglevel = DEBUG
+                   #loglevel = DEBUG
+                   #log-config-on-start = on
                     }
-                   akka.actor{                     
+                   akka.actor{
+                      provider= ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
                       debug {
-			                receive = on
-				            autoreceive = on
-				            lifecycle = on
-				            event-stream = on
-				            unhandled = on
+			                #receive = on
+				            #autoreceive = on
+				            #lifecycle = on
+				            #event-stream = on
+				            #unhandled = on
 		            }
+                   }
+                  akka.remote {
+                      #log-remote-lifecycle-events = DEBUG
+                      #log-received-messages = on
+                      #log-sent-messages = on
+                    helios.tcp {
+                      #log-remote-lifecycle-events = DEBUG
+                      #log-received-messages = on
+                      #log-sent-messages = on
+                      transport-class =""Akka.Remote.Transport.Helios.HeliosTcpTransport, Akka.Remote""
+                      port = 0
+                      transport-protocol = tcp
+                      hostname = ""localhost""
+                     }
                    }
               ");
             }
--- build-server.fsx
+++ build-server.fsx
@@ -57,7 +57,7 @@ let NugetDeployPath= match nugetDeployPath with
 let version =
   match buildServer with
   | TeamCity -> (buildVersion+BuildVersionType)
-  | _        -> ("0.9.9"+BuildVersionType)
+  | _        -> ("1.0.0"+BuildVersionType)
 
 // Targets
 Target "Clean" (fun _ -> 
