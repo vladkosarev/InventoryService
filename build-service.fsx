01f2d607b21916f76e34283103f62cf33ab4f152
Sam <contactsamie@gmail.com>
2016-11-09 14:14:58 -0500

    update builds

--- build-service.fsx
+++ build-service.fsx
@@ -63,7 +63,7 @@ let NugetDeployPath= match nugetDeployPath with
 let version =
   match buildServer with
   | TeamCity -> (buildVersion+BuildVersionType)
-  | _        -> ("1.0.3"+BuildVersionType)
+  | _        -> ("1.0.4"+BuildVersionType)
 
 // Targets
 Target "Clean" (fun _ -> 
